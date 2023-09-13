2f9e1558aa30b7346c9203919b4162f21cde9ada
curiousdevlab <hello@curiousdevlab.com>
2023-09-12 20:26:37 -0400

    ignore ssr build assets

--- bootstrap/ssr/assets/CacheLink-e7404a8b.js
+++
@@ -1,8 +0,0 @@
-import { jsx } from "react/jsx-runtime";
-import { Link } from "@inertiajs/react";
-const CacheLink = ({ children, isStatic, ...props }) => {
-  return /* @__PURE__ */ jsx(Link, { ...props, children });
-};
-export {
-  CacheLink as C
-};
